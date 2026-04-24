3c53fba6e363bced4b84fa08a8a21de2546987a1
Ben Palmer <b.c.palmer@northumbria.ac.uk>
2026-04-24 10:58:29 +0100

    Created new ingest algorithm

---
+++ src/clev2er/algorithms/seaice_stage_3_maps/alg_ingest_along_track.py
@@ -0,0 +1,182 @@
+"""clev2er.algorithms.seaice.alg_ingest_along_track.py
+
+Algorithm class module, used to implement a single chain algorithm
+
+#Description of this Algorithm's purpose
+
+Ingest data from along track input file and add it to shared dictionary
+
+#Main initialization (init() function) steps/resources required
+
+Get variables to be ingested from config
+
+#Main process() function steps
+
+For each variable in config:
+    Read variable data
+    Apply filtering
+    Add to shared dict
+
+#Main finalize() function steps
+
+None
+
+#Contribution to shared_dict
+
+Variable as named in config
+
+#Requires from shared_dict
+
+None
+
+Author: Ben Palmer
+Date: 23 Feb 2026
+"""
+
+from typing import Tuple
+
+from codetiming import Timer
+from netCDF4 import Dataset  # pylint:disable=no-name-in-module
+
+from clev2er.algorithms.base.base_alg import BaseAlgorithm
+
+
+class Algorithm(BaseAlgorithm):
+    """CLEV2ER Algorithm class
+
+    contains:
+            .log (Logger) : log instance that must be used for all logging, set by BaseAlgorithm
+            .config (dict) : configuration dictionary, set by BaseAlgorithm
+
+        functions that need completing:
+            .init() : Algorithm initialization function (run once at start of chain)
+            .process(l1b,shared_dict) : Algorithm processing function (run on every L1b file)
+            .finalize() :   Algorithm finalization/closure function (run after all chain
+                            processing completed)
+
+        Inherits from BaseAlgorithm which handles interaction with the chain controller run_chain.py
+
+    """
+
+    def init(self) -> Tuple[bool, str]:
+        """Algorithm initialization function
+
+        Add steps in this function that are run once at the beginning of the chain
+        (for example loading a DEM or Mask)
+
+        Returns:
+            (bool,str) : success or failure, error string
+
+        Test for KeyError or OSError exceptions and raise them if found
+        rather than just returning (False,"error description")
+
+        Raises:
+            KeyError : for keys not found in self.config
+            OSError : for any file related errors
+
+        Note:
+        - retrieve required config data from self.config dict
+        - log using self.log.info(), or self.log.error() or self.log.debug()
+
+        """
+        # pylint:disable=pointless-string-statement
+        self.alg_name = __name__
+        self.log.info("Algorithm %s initializing", self.alg_name)
+
+        # --- Add your initialization steps below here ---
+
+        """ 
+        Get config parameters
+        Check that output directory exists
+        """
+
+        self.variables = self.config["alg_ingest_along_track"]["variables"]
+
+        # --- End of initialization steps ---
+
+        return (True, "")
+
+    @Timer(name=__name__, text="", logger=None)
+    def process(
+        self,
+        l1b: Dataset,
+        shared_dict: dict,  # pylint:disable=unused-argument
+    ) -> Tuple[bool, str]:
+        # pylint: disable=too-many-locals
+        # pylint: disable=unpacking-non-sequence
+        # pylint:disable=pointless-string-statement
+        """Main algorithm processing function, called for every L1b file
+
+        Args:
+            l1b (Dataset): input l1b file dataset (constant)
+            shared_dict (dict): shared_dict data passed between algorithms. Use this dict
+                                to pass algorithm results down the chain or read variables
+                                set by other algorithms.
+
+        Returns:
+            Tuple : (success (bool), failure_reason (str))
+            ie
+            (False,'error string'), or (True,'')
+
+        Note:
+        - retrieve required config data from self.config dict (read-only)
+        - retrieve data from other algorithms from shared_dict
+        - add results,variables from this algorithm to shared_dict
+        - log using self.log.info(), or self.log.error() or self.log.debug()
+
+        """
+
+        # This step is required to support multi-processing. Do not modify
+        success, error_str = self.process_setup(l1b)
+        if not success:
+            return (False, error_str)
+
+        # -------------------------------------------------------------------
+        # Perform the algorithm processing, store results that need to be passed
+        # /    down the chain in the 'shared_dict' dict     /
+        # -------------------------------------------------------------------
+
+        """ 
+        For each variable in config:
+            Read variable data
+            Apply filtering if necessary
+            Add to shared dict
+        """
+
+        for var_name in self.variables:
+            if var_name not in l1b.variables:
+                raise RuntimeError(f"Cannot find variable {var_name} in input file.")
+
+            data = l1b[var_name][:].data
+
+            shared_dict[var_name] = data
+
+        # -------------------------------------------------------------------
+        # Returns (True,'') if success
+        return (success, error_str)
+
+    def finalize(self, stage: int = 0) -> None:
+        """Algorithm finalization function - called after all processing completed
+
+        Can be used to clean up/free resources initialized in the init() function
+
+        Args:
+            stage (int, optional):  this sets the stage when this function is called
+                                    by the chain controller. Useful during multi-processing.
+                                    Defaults to 0. Not normally used by Algorithms.
+        """
+        self.log.info(
+            "Finalize algorithm %s called at stage %d filenum %d",
+            self.alg_name,
+            stage,
+            self.filenum,
+        )
+        # pylint:disable=pointless-string-statement
+
+        # ---------------------------------------------------------------------
+        # Add finalization steps here /
+        # ---------------------------------------------------------------------
+
+        """ None """
+
+        # ---------------------------------------------------------------------
